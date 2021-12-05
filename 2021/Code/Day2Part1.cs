7ca669bb3dceaf142007eb6a42c82ea276ddf26b
Jake Boraman <jake.boraman@gmail.com>
2021-12-05 14:45:35 +0000

    Added my solution for 2021 day 2 part 1

---
+++ 2021/Code/Day2Part1.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace subPilot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = System.IO.File.ReadAllLines(@"movements.txt");
+            int result = positionFinder(inputs);
+            Console.WriteLine("Answer: " + result);
+            Console.ReadKey();
+        }
+
+        static int positionFinder(string[] commands)
+        {
+            int output = 0;
+            int horizPos = 0;
+            int depthPos = 0;
+
+            foreach (string command in commands)
+            {
+                int indexOfSpace = command.IndexOf(' ');
+                string direction = command.Substring(0, indexOfSpace);
+                int number = Int32.Parse(command.Substring(indexOfSpace + 1, 1));
+
+                switch (direction)
+                {
+                    case "forward":
+                        horizPos += number;
+                        break;
+
+                    case "up":
+                        depthPos -= number;
+                        break;
+
+                    case "down":
+                        depthPos += number;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            output = horizPos * depthPos;
+
+            return output;
+        }
+    }
+}
