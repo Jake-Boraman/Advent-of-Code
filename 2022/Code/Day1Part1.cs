d88cbf911627c182f2168b39ab6300563f4a4ea8
Jake Boraman <jake.boraman@gmail.com>
2023-01-11 19:03:49 +0000

    Added my solution for 2022 day 1 part 1

--- 2022/Code/Day1Part1.cs
+++ 2022/Code/Day1Part1.cs
@@ -7,7 +7,48 @@ namespace CalorieCounting
     {
         static void Main(string[] args)
         {
+            // Variables
+            int maxCals = 0;
+            List<string> tempElf = new List<string>();
+            List<string[]> elves = new List<string[]>();
+            string[] inputs = System.IO.File.ReadAllLines(@"calories.txt");
+
+            // Elf array grabber
+            foreach (string input in inputs){
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    // Add to current array
+                    tempElf.Add(input);
+                }
+                else
+                {
+                    // Current elf has ended, add array to list, clear array
+                    elves.Add(tempElf.ToArray());
+                    tempElf.Clear();
+                }
+            }
             
+            // Calorie Counter
+            foreach (string[] elf in elves)
+            {
+                int currentTotal = 0;
+                foreach (string foodItem in elf)
+                {
+                    currentTotal += Int32.Parse(foodItem);
+                }
+
+                if (currentTotal > maxCals)
+                {
+                    maxCals = currentTotal;
+                    Console.WriteLine("New Calorie Record!");
+                }
+                else
+                {
+                    Console.WriteLine("Record not broken");
+                }
+            }
+
+            Console.WriteLine("The elf with the highest calories has " + maxCals + " calories!");
         }
     }
 }
