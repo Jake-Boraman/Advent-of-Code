3180ac3dd10e708d86560e1aeaa32103ee75bd76
Jake Boraman <jake.boraman@gmail.com>
2023-01-11 12:46:31 +0000

    Added 2019

---
+++ 2019/Code/Day1Part1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] modules = System.IO.File.ReadAllLines("Day1.txt");
+            int totalFuel = 0;
+
+        }
+    }
+}
