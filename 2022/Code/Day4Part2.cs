be32a949322e3f8bcfdb867e077ce94dbab874d0
Jake Boraman <jake.boraman@gmail.com>
2023-01-26 16:36:31 +0000

    Added my solution for 2022 day 4 part 2

---
+++ 2022/Code/Day4Part2.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+
+namespace campCleanup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = System.IO.File.ReadAllLines("input.txt");
+            int totalOverlappingPairs = 0;
+
+            foreach (string line in inputs)
+            {
+                List<int> elf1 = new List<int>();
+                List<int> elf2 = new List<int>();
+                string[] separate = line.Split(',');
+
+                int elf1DashLoc = separate[0].IndexOf('-');
+                int elf2DashLoc = separate[1].IndexOf('-');
+                int elf1start = Int32.Parse(separate[0].Substring(0, elf1DashLoc));
+                int elf1end = Int32.Parse(separate[0].Substring(elf1DashLoc + 1, separate[0].Length - elf1DashLoc - 1));
+                int elf2start = Int32.Parse(separate[1].Substring(0, elf2DashLoc));
+                int elf2end = Int32.Parse(separate[1].Substring(elf2DashLoc + 1, separate[1].Length - elf2DashLoc - 1));
+
+                for (int i = elf1start; i <= elf1end; i++)
+                {
+                    elf1.Add(i);
+                }
+
+                for (int i = elf2start; i <= elf2end; i++)
+                {
+                    elf2.Add(i);
+                }
+
+                if (elf1.Intersect(elf2).Any())
+                {
+                    totalOverlappingPairs += 1;
+                }
+            }
+            Console.WriteLine("There are " + totalOverlappingPairs + " pairs that match the conditions");
+        }
+
+        public static bool ContainsAllItemsAinB(List<int> a, List<int> b)
+        {
+            return !a.Except(b).Any();
+        }
+
+        public static bool ContainsAllItemsBinA(List<int> a, List<int> b)
+        {
+            return !b.Except(a).Any();
+        }
+    }
+}
