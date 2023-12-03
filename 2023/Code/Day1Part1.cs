6962bfe5063a8f132a382cd84eadbe6e61d583f3
Jake Boraman <jake.boraman@gmail.com>
2023-12-03 15:53:10 +0000

    Added my solution for 2023 day 1 part 1

---
+++ 2023/Code/Day1Part1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TrebuchetCalibration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("I:\\C#\\adventofcode\\2023\\Code\\input.txt");
+            int[] calib_values = new int[lines.Length];
+            int i = 0;
+
+            foreach (string line in lines)
+            {
+                List<char> chars = new List<char>();
+
+                foreach (char c in line)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        chars.Add(c);
+                    }
+                }
+
+                calib_values[i] = Int32.Parse(chars[0].ToString() + chars[chars.Count() - 1].ToString());
+                i++;
+            }
+
+            int total = calib_values.Sum();
+            Console.WriteLine($"The sum of these calibration values is {total}.");
+        }
+    }
+}
